3a8bbc34939bd30b35759f456450c962df0f3251
Khang Vo <ktabe2006@gmail.com>
2025-06-17 19:38:10 +0700

    Update CategoryRepository.java
    add chuc nang dem san pham cua danh muc

--- ASM/src/main/java/com/poly/asm/daos/CategoryRepository.java
+++ ASM/src/main/java/com/poly/asm/daos/CategoryRepository.java
@@ -2,6 +2,7 @@ package com.poly.asm.daos;
 
 import com.poly.asm.entitys.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
@@ -11,4 +12,7 @@ public interface CategoryRepository extends JpaRepository<Category, Long> {
 
     // Thêm phương thức tìm theo tên (tùy chọn)
     Category findByName(String name);
+    
+    @Query("SELECT COUNT(p) FROM Product p WHERE p.category.id = ?1")
+    Long countProductsByCategoryId(Long categoryId);
 }
